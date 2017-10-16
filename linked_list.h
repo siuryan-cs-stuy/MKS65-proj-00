8eb839759f1bba3c75bdeb26e245828e7bd997c8
Ryan Siu <rsiu@stuy.edu>
2017-10-16 15:30:37 -0400

    Add linked list function headers

--- linked_list.h
+++ linked_list.h
@@ -1,8 +1,14 @@
 struct node {
-  int i;
+  char name[256];
+  char artist[256];
   struct node *next;
 };
 
 void print_list(struct node *);
-struct node * insert_front(struct node *, int);
+struct node * insert_front(struct node *, char, char);
 struct node * free_list(struct node *);
+struct node * insert_order(struct node *, char, char);
+struct node * find_song(struct node *, char);
+struct node * find_artist(struct node *, char);
+struct node * random_node(struct node *);
+struct node * remove_node(struct node *);
