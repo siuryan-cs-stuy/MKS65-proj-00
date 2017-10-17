fe114cd5bd5e301c79502529320da41d065598d0
Ryan Siu <rsiu@stuy.edu>
2017-10-17 14:58:12 -0400

    Change char to char *

--- linked_list.h
+++ linked_list.h
@@ -5,10 +5,10 @@ struct node {
 };
 
 void print_list(struct node *);
-struct node * insert_front(struct node *, char, char);
+struct node * insert_front(struct node *, char *, char *);
 struct node * free_list(struct node *);
-struct node * insert_order(struct node *, char, char);
-struct node * find_song(struct node *, char);
-struct node * find_artist(struct node *, char);
+struct node * insert_order(struct node *, char *, char *);
+struct node * find_song(struct node *, char *);
+struct node * find_artist(struct node *, char *);
 struct node * random_node(struct node *);
 struct node * remove_node(struct node *);
